35dbdcea1093206d62aab8c8610cd418f2bb9401
abbyd <abbydiaz300@gmail.com>
2022-09-05 16:24:34 -0700

    played with console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,16 @@
 
  var playerName = window.prompt('What is your robots name?');
+console.log(playerName);
+
+console.log('a string is just a way to say txt thats inside the quotes');
+//this one does math
+console.log(10 + 10);
+//this one combines data from a var with a string 
+console.log('My Robots name is ' + playerName);
 
 // fight function
 
 function fight() {
     window.alert('The fight has begun!');
 }
-fight()
-
-//stopping in 3.1.5s
+//fight()
