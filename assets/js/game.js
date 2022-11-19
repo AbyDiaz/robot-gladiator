d4bbbb1e5a3d25934fa9ee02387a17187d65a78a
abbyd <abbydiaz300@gmail.com>
2022-11-19 14:40:10 -0800

    added funtion getPlayerName as to not accept blank or null input

--- assets/js/game.js
+++ assets/js/game.js
@@ -79,7 +79,7 @@ var startGame = function() {
     // if player is still alive, keep fight next enemy
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));  
 
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
@@ -167,11 +167,32 @@ var shop = function() {
 
 /* END GAME FUNCTIONS */
 
+// function to set name 
+
+var getPlayerName = function() {
+  var name = '';
+
+  while (name === '' || name === null) {
+    name = window.prompt('what is your robots name?');
+
+    // prompt player until valid data is recieved
+    if (name === '' || name === null) {
+      window. alert ('You need to put a real name silly!');
+    }
+    
+  }
+
+  console.log('Your robots name is ' + name);
+  return name;
+
+};
+
 /* GAME INFORMATION / VARIABLES */
 
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  //name: window.prompt("What is your robot's name?"),
+  name : getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
